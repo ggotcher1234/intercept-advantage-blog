b9c87eb3f48acd3f06b7ed910e7cd407efc57da2
Greg <63166579+ggotcher1234@users.noreply.github.com>
2026-07-21 11:16:05 -0700

    Fix syntax error in eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
-module.exports = function (eleventyConfig) {
+module.exports = function (eleventyConfig) {  eleventyConfig.addPassthroughCopy("src/admin");
+
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/images");
 
